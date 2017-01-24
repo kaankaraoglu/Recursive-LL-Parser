5335716573aea7a3cf10bc970f29af82fb454df3
Kaan Karaoglu <raonn@users.noreply.github.com>
2017-01-24 20:34:27 +0100

    Beautified code

--- src/RecursiveLLParser.java
+++ src/RecursiveLLParser.java
@@ -10,9 +10,8 @@ public class RecursiveLLParser {
 	public static int index = 0;
 	public static int charClass;
 	public static String lexeme = "";
-	public static ArrayList<Character> symbols = new ArrayList<Character>(
-			Arrays.asList('[', ']', '{', '}', '(', ')', '<', '>', '\'', '"',
-					'=', '|', '.', ',', ';'));
+	public static ArrayList < Character > symbols = new ArrayList < Character > (
+	Arrays.asList('[', ']', '{', '}', '(', ')', '<', '>', '\'', '"', '=', '|', '.', ',', ';'));
 	public static char nextChar;
 	public static int nextToken;
 
@@ -60,7 +59,7 @@ public class RecursiveLLParser {
 			addChar();
 			nextToken = LEFT_BRACK;
 			break;
-		/* RIGHT ARROW CASE */
+
 		case '%':
 			addChar();
 			nextToken = RIGHT_ARROW;
@@ -152,37 +151,30 @@ public class RecursiveLLParser {
 	private static void addChar() {
 		if (lexeme.length() <= 98) {
 			lexeme += nextChar;
-		} else
-			System.out.println("Error - lexeme is too long \n");
+		} else System.out.println("Error - lexeme is too long \n");
 	}
 
 	private static void getChar() {
 		if (input.length() > index) {
 			nextChar = input.charAt(index++);
-			if (Character.isAlphabetic(nextChar))
-				charClass = LETTER;
-			else if (Character.isDigit(nextChar))
-				charClass = DIGIT;
-			else if (symbols.contains(nextChar))
-				charClass = SYMBOL;
-			else if (nextChar == '_')
-				charClass = UNDERLINE;
-			else
-				charClass = UNKNOWN;
-		} else
-			charClass = EOF;
+			if (Character.isAlphabetic(nextChar)) charClass = LETTER;
+			else if (Character.isDigit(nextChar)) charClass = DIGIT;
+			else if (symbols.contains(nextChar)) charClass = SYMBOL;
+			else if (nextChar == '_') charClass = UNDERLINE;
+			else charClass = UNKNOWN;
+		} else charClass = EOF;
 	}
 
 	private static void getNonBlank() {
 		while (Character.isWhitespace(nextChar))
-			getChar();
+		getChar();
 	}
 
 	private static int lex() {
 		lexeme = "";
 		getNonBlank();
 		switch (charClass) {
-		/* Parse identifiers */
+			/* Parse identifiers */
 		case LETTER:
 			addChar();
 			getChar();
@@ -190,8 +182,7 @@ public class RecursiveLLParser {
 				nextToken = CHARACTER;
 				break;
 			}
-			while (charClass == LETTER || charClass == DIGIT
-					|| charClass == UNDERLINE) {
+			while (charClass == LETTER || charClass == DIGIT || charClass == UNDERLINE) {
 				addChar();
 				getChar();
 			}
@@ -201,14 +192,11 @@ public class RecursiveLLParser {
 		case DIGIT:
 			addChar();
 			getChar();
-			if (Character.isWhitespace(nextChar))
-				nextToken = CHARACTER;
-			else
-				System.out
-						.println("SYNTAX ERROR: Input is neither Character nor Identifier");
+			if (Character.isWhitespace(nextChar)) nextToken = CHARACTER;
+			else System.out.println("SYNTAX ERROR: Input is neither Character nor Identifier");
 			break;
 
-		/* Symbols */
+			/* Symbols */
 		case SYMBOL:
 			lookup(nextChar);
 			getChar();
@@ -223,21 +211,21 @@ public class RecursiveLLParser {
 			nextToken = EOF;
 			lexeme = "EOF";
 
-		} /* End of switch */
-		System.out.println("\tNext token is: " + nextToken + " Next lexeme is "
-				+ lexeme.toString());
+		}
+		/* End of switch */
+		System.out.println("\tNext token is: " + nextToken + " Next lexeme is " + lexeme.toString());
 		return nextToken;
 
 	}
 
 	private static void grammar() {
 		System.out.println("Entering grammar...");
-		
-		do{
+
+		do {
 			lex();
 			rule();
-		}while(nextToken == SEMICOLON);
-	
+		} while ( nextToken == SEMICOLON );
+
 		System.out.println("Exiting grammar...");
 	}
 
@@ -246,44 +234,32 @@ public class RecursiveLLParser {
 		lhs();
 		if (nextToken == RIGHT_ARROW) {
 			lex();
-			if (nextToken != EOF)
-				rhs();
-			else
-				System.out.println("RULE ERROR: RHS Statement is missing.");
-		} else
-			System.out.println("Rule : Missing right arrow.");
+			if (nextToken != EOF) rhs();
+			else System.out.println("RULE ERROR: RHS Statement is missing.");
+		} else System.out.println("Rule : Missing right arrow.");
 		System.out.println("Exiting rule...");
 	}
 
 	private static void rhs() {
 		System.out.println("Entering RHS...");
-		if (nextToken == IDENT)
-			lex();
+		if (nextToken == IDENT) lex();
 		else if (nextToken == LEFT_BRACK) {
 			lex();
 			rhs();
-			if (nextToken == RIGHT_BRACK)
-				lex();
-			else
-				System.out.println("RHS ERROR: Missing Right bracket.");
-		} else if (nextToken == APOST)
-			terminal();
-		else if (nextToken == QUOTA)
-			terminal();
+			if (nextToken == RIGHT_BRACK) lex();
+			else System.out.println("RHS ERROR: Missing Right bracket.");
+		} else if (nextToken == APOST) terminal();
+		else if (nextToken == QUOTA) terminal();
 		else if (nextToken == LEFT_CURLY) {
 			lex();
 			rhs();
-			if (nextToken == RIGHT_CURLY)
-				lex();
-			else
-				System.out.println("RHS ERROR: Missing Right Curly Braces.");
+			if (nextToken == RIGHT_CURLY) lex();
+			else System.out.println("RHS ERROR: Missing Right Curly Braces.");
 		} else if (nextToken == LEFT_PAREN) {
 			lex();
 			rhs();
-			if (nextToken == RIGHT_PAREN)
-				lex();
-			else
-				System.out.println("RHS ERROR: Missing Right Parantheses.");
+			if (nextToken == RIGHT_PAREN) lex();
+			else System.out.println("RHS ERROR: Missing Right Parantheses.");
 		}
 		if (nextToken == OR_SYMBOL) {
 			lex();
@@ -299,36 +275,27 @@ public class RecursiveLLParser {
 			if (nextToken == CHARACTER) {
 				lex();
 				while (nextToken == CHARACTER)
-					lex();
-				if (nextToken == APOST)
-					lex();
-				else
-					System.out.println("TERMINAL Error: Missing Apostrophe.");
-			} else
-				System.out.println("TERMINAL Error: Expecting Character...");
+				lex();
+				if (nextToken == APOST) lex();
+				else System.out.println("TERMINAL Error: Missing Apostrophe.");
+			} else System.out.println("TERMINAL Error: Expecting Character...");
 		} else if (nextToken == QUOTA) {
 			lex();
 			if (nextToken == CHARACTER) {
 				lex();
 				while (nextToken == CHARACTER)
-					lex();
-				if (nextToken == QUOTA)
-					lex();
-				else
-					System.out
-							.println("TERMINAL Error: Missing Quoatation symbol.");
-			} else
-				System.out.println("TERMINAL Error: Expecting character...");
+				lex();
+				if (nextToken == QUOTA) lex();
+				else System.out.println("TERMINAL Error: Missing Quoatation symbol.");
+			} else System.out.println("TERMINAL Error: Expecting character...");
 		}
 		System.out.println("Exiting Terminal...");
 	}
 
 	private static void lhs() {
 		System.out.println("Entering LHS...");
-		if (nextToken == IDENT)
-			lex();
-		else
-			System.out.println("LHS Error: Expecting Identifier");
+		if (nextToken == IDENT) lex();
+		else System.out.println("LHS Error: Expecting Identifier");
 		System.out.println("Exiting LHS...");
 	}
 }
